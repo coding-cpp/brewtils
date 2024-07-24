2a5e338d3baefa46652dc2da6ed485cc36030823
Adit Jain <77121346+Jadit19@users.noreply.github.com>
2024-07-24 10:42:13 +0530

    update env parsing logic

--- src/env.cpp
+++ src/env.cpp
@@ -1,29 +1,29 @@
 #include <brewtils/env.h>
 
-void brewtils::env::init(std::string path) noexcept(true) {
+void brewtils::env::init(std::string path) noexcept(false) {
   if (brewtils::os::dir::exists(path)) {
     path = brewtils::os::joinPath(path, ".env");
   }
 
   if (!brewtils::os::file::exists(path)) {
-    logger::warning("No .env file found at " + path);
+    logger::error("No .env file found at " + path);
     return;
   }
 
   const std::string data = brewtils::os::file::read(path);
+  std::string key, value;
   for (const std::string &line : brewtils::string::split(data, "\n")) {
-    const std::vector<std::string> parts = brewtils::string::split(line, "=");
-    if (parts.size() == 1 && parts[0].empty()) {
+    std::size_t pos = line.rfind('=');
+    if (pos == std::string::npos) {
       continue;
     }
 
-    if (parts.size() != 2) {
-      logger::warning("Invalid .env line: " + line);
-      continue;
-    }
-
-    brewtils::env::set(parts[0], parts[1]);
+    key = line.substr(0, pos);
+    value = line.substr(pos + 1);
+    brewtils::env::set(key, value);
   }
+
+  return;
 }
 
 std::string brewtils::env::get(const std::string &key) noexcept(true) {
@@ -31,12 +31,20 @@ std::string brewtils::env::get(const std::string &key) noexcept(true) {
 }
 
 void brewtils::env::set(const std::string &key,
-                        const std::string &value) noexcept(true) {
+                        const std::string &value) noexcept(false) {
   if (value.empty()) {
     logger::warning("Unsetting environment variable " + key);
-    unsetenv(key.c_str());
+    if (unsetenv(key.c_str()) != 0) {
+      logger::error("Failed to unset environment variable " + key,
+                    "void set(const std::string &key, const std::string "
+                    "&value) noexcept(false)");
+    }
   } else {
-    setenv(key.c_str(), value.c_str(), 1);
+    if (setenv(key.c_str(), value.c_str(), 1) != 0) {
+      logger::error("Failed to set environment variable " + key,
+                    "void set(const std::string &key, const std::string "
+                    "&value) noexcept(false)");
+    }
   }
 
   return;
